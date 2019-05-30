9b43d9d200df2f4ce26af48754f951446b1b2f6e
SBobak <bobak.sandra@gmail.com>
2019-05-30 20:11:50 +0200

    A więc działa większość,,,,,,,,,,,,,

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -6,8 +6,11 @@ public class Ball : MonoBehaviour
 {
 
     [SerializeField] Paddle paddle1;
-
-    Vector2 paddleToBallVector;
+    [SerializeField] float xPush;
+    [SerializeField] float yPush;
+    [SerializeField] bool hasStarted = false;
+    [SerializeField] Rigidbody2D rb;
+     Vector2 paddleToBallVector;
 
     private void Start()
     {
@@ -17,6 +20,26 @@ public class Ball : MonoBehaviour
     }
 
     private void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchToPaddle();
+        }
+    }
+
+    private void LaunchToPaddle()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            rb.simulated = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+
+    private void LockBallToPaddle()
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
